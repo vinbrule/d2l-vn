99919a6349e7641b0c508b0dbc4760368e883b16
Tiep Vu <vuhuutiep@gmail.com>
2019-12-15 18:09:11 -0800

    wip

--- deploy.sh
+++ deploy.sh
@@ -32,21 +32,21 @@ DIR=${DIR}_tmp
 # find $DIR \( -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
 
 # --content-encoding 'gzip' \
-aws s3 sync --exclude '*.*' --include '*.css' \
+python3 -m aws s3 sync --exclude '*.*' --include '*.css' \
      --content-type 'text/css' \
      --acl 'public-read'\
      $DIR $BUCKET
 
 # --content-encoding 'gzip' \
-aws s3 sync --exclude '*.*' --include '*.js' \
+python3 -m aws s3 sync --exclude '*.*' --include '*.js' \
      --content-type 'application/javascript' \
      --acl 'public-read'\
      $DIR $BUCKET
 
 # use a large expire time for fonts
-aws s3 sync --exclude '*.*' --include '*.woff' --include '*.woff2' \
+python3 -m aws s3 sync --exclude '*.*' --include '*.woff' --include '*.woff2' \
      --expires "2021-11-28T00:00:01Z" \
      --acl 'public-read'\
      $DIR $BUCKET
 
-aws s3 sync --delete $DIR $BUCKET --acl 'public-read'
+python3 -m aws s3 sync --delete $DIR $BUCKET --acl 'public-read'
